d56026c8cc6046b7d954001d93e9b72adf4403e1
Prad <prnk28@gmail.com>
2020-09-24 18:31:21 -0400

    Add methods for HiveDB connector

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import 'package:hive/hive.dart';
 import 'package:sonar_app/bloc/bloc.dart';
 import 'package:sonar_app/core/core.dart';
 import 'package:sonar_app/screens/screens.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main() async {
   // Set bloc observer to observe transitions
   Bloc.observer = SimpleBlocObserver();
+
+  // Initialize HiveDB
+  await Hive.initFlutter();
 
   // Run App with BLoC Providers
   runApp(MultiBlocProvider(
--- lib/repository/local_data.dart
+++ lib/repository/local_data.dart
@@ -1,5 +1,39 @@
 import 'package:hive/hive.dart';
 
 class LocalData {
-  
+  // ** -- Class Constructer -- **
+  LocalData();
+
+  // ******************************** //
+  // ** Add/Update Persistent Data ** //
+  // ******************************** //
+  void addContact() {}
+
+  void addFileMetadata() {}
+
+  void updatePreferances() {}
+
+  void updateProfile() {}
+
+  // ****************************** //
+  // ** Retrieve Persistent Data ** //
+  // ****************************** //
+  void getContact() {}
+
+  void getFileMetadata() {}
+
+  void getPreferances() {}
+
+  void getProfile() {}
+
+  // **************************** //
+  // ** Delete Persistent Data ** //
+  // **************************** //
+  void deleteContact() {}
+
+  void deleteFileMetadata() {}
+
+  void deletePreferances() {}
+
+  void deleteProfile() {}
 }
