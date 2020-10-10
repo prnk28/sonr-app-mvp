f95fab5ca3843853888215894e4697aa0866c208
Prad <prnk28@gmail.com>
2020-10-10 13:03:30 -0400

    Create Compass Ticker

--- lib/screens/transfer/widgets/compass.dart
+++ lib/screens/transfer/widgets/compass.dart
@@ -13,141 +13,170 @@ class CompassView extends StatelessWidget {
   const CompassView({Key key, this.user}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1,
-      child: Neumorphic(
-        margin: EdgeInsets.all(14),
-        style: NeumorphicStyle(
-          boxShape: NeumorphicBoxShape.circle(),
-        ),
+    return Stack(alignment: Alignment.topCenter, children: [
+      // Compass Total
+      AspectRatio(
+        aspectRatio: 1,
         child: Neumorphic(
+          margin: EdgeInsets.all(14),
           style: NeumorphicStyle(
-            depth: 14,
             boxShape: NeumorphicBoxShape.circle(),
           ),
-          margin: EdgeInsets.all(20),
           child: Neumorphic(
             style: NeumorphicStyle(
-              depth: -8,
+              depth: 14,
               boxShape: NeumorphicBoxShape.circle(),
             ),
-            margin: EdgeInsets.all(10),
-            child: Stack(
-              fit: StackFit.expand,
-              alignment: Alignment.center,
-              children: [
-                // Center Circle
-                Neumorphic(
-                  style: NeumorphicStyle(
-                    depth: -3,
-                    boxShape: NeumorphicBoxShape.circle(),
+            margin: EdgeInsets.all(20),
+            child: Neumorphic(
+              style: NeumorphicStyle(
+                depth: -8,
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              margin: EdgeInsets.all(10),
+              child: Stack(
+                fit: StackFit.expand,
+                alignment: Alignment.center,
+                children: [
+                  // Center Circle
+                  Neumorphic(
+                    style: NeumorphicStyle(
+                      depth: -3,
+                      boxShape: NeumorphicBoxShape.circle(),
+                    ),
+                    margin: EdgeInsets.all(70),
                   ),
-                  margin: EdgeInsets.all(70),
-                ),
-                // Spokes
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Stack(
-                    children: <Widget>[
-                      // ** <North> **//
-                      _buildMajorSpoke(0,
-                          isNegativeAlignment: true,
-                          textColor: Colors.red[900],
-                          textValue: "N",
-                          textPadding:
-                              EdgeInsets.only(bottom: _K_MAJOR_BOTTOM_PADDING)),
-                      // ** </North> **//
+                  // Spokes
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Stack(
+                      children: <Widget>[
+                        // ** <North> **//
+                        _buildMajorSpoke(0,
+                            isNegativeAlignment: true,
+                            textColor: Colors.red[900],
+                            textValue: "N",
+                            textPadding: EdgeInsets.only(
+                                bottom: _K_MAJOR_BOTTOM_PADDING)),
+                        // ** </North> **//
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(11.25),
-                      _buildMinorSpoke(22.5),
-                      _buildMinorSpoke(33.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(11.25),
+                        _buildMinorSpoke(22.5),
+                        _buildMinorSpoke(33.75),
+                        // </MinorSpokes> //
 
-                      // NorthEast
-                      _buildAuxiliarySpoke(45),
+                        // NorthEast
+                        _buildAuxiliarySpoke(45),
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(56.25),
-                      _buildMinorSpoke(67.5),
-                      _buildMinorSpoke(78.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(56.25),
+                        _buildMinorSpoke(67.5),
+                        _buildMinorSpoke(78.75),
+                        // </MinorSpokes> //
 
-                      // ** <East> **//
-                      _buildMajorSpoke(90,
-                          textValue: "W",
-                          textPadding:
-                              EdgeInsets.only(top: _K_MAJOR_TOP_PADDING)),
-                      // ** </East> **//
+                        // ** <East> **//
+                        _buildMajorSpoke(90,
+                            textValue: "W",
+                            textPadding:
+                                EdgeInsets.only(top: _K_MAJOR_TOP_PADDING)),
+                        // ** </East> **//
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(101.25),
-                      _buildMinorSpoke(112.5),
-                      _buildMinorSpoke(123.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(101.25),
+                        _buildMinorSpoke(112.5),
+                        _buildMinorSpoke(123.75),
+                        // </MinorSpokes> //
 
-                      // SouthEast
-                      _buildAuxiliarySpoke(135),
+                        // SouthEast
+                        _buildAuxiliarySpoke(135),
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(146.25),
-                      _buildMinorSpoke(157.5),
-                      _buildMinorSpoke(168.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(146.25),
+                        _buildMinorSpoke(157.5),
+                        _buildMinorSpoke(168.75),
+                        // </MinorSpokes> //
 
-                      // ** <South> **//
-                      _buildMajorSpoke(180,
-                          isNegativeAlignment: true,
-                          textValue: "S",
-                          textPadding:
-                              EdgeInsets.only(bottom: _K_MAJOR_BOTTOM_PADDING)),
-                      // ** </South> **//
+                        // ** <South> **//
+                        _buildMajorSpoke(180,
+                            isNegativeAlignment: true,
+                            textValue: "S",
+                            textPadding: EdgeInsets.only(
+                                bottom: _K_MAJOR_BOTTOM_PADDING)),
+                        // ** </South> **//
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(191.25),
-                      _buildMinorSpoke(202.5),
-                      _buildMinorSpoke(213.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(191.25),
+                        _buildMinorSpoke(202.5),
+                        _buildMinorSpoke(213.75),
+                        // </MinorSpokes> //
 
-                      // SouthWest
-                      _buildAuxiliarySpoke(225),
+                        // SouthWest
+                        _buildAuxiliarySpoke(225),
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(236.25),
-                      _buildMinorSpoke(247.5),
-                      _buildMinorSpoke(258.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(236.25),
+                        _buildMinorSpoke(247.5),
+                        _buildMinorSpoke(258.75),
+                        // </MinorSpokes> //
 
-                      // ** <West> **//
-                      _buildMajorSpoke(270,
-                          textValue: "E",
-                          textPadding:
-                              EdgeInsets.only(top: _K_MAJOR_TOP_PADDING)),
-                      // ** </West> **//
+                        // ** <West> **//
+                        _buildMajorSpoke(270,
+                            textValue: "E",
+                            textPadding:
+                                EdgeInsets.only(top: _K_MAJOR_TOP_PADDING)),
+                        // ** </West> **//
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(281.25),
-                      _buildMinorSpoke(292.5),
-                      _buildMinorSpoke(303.75),
-                      // </MinorSpokes> //
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(281.25),
+                        _buildMinorSpoke(292.5),
+                        _buildMinorSpoke(303.75),
+                        // </MinorSpokes> //
 
-                      // NorthWest
-                      _buildAuxiliarySpoke(315),
+                        // NorthWest
+                        _buildAuxiliarySpoke(315),
 
-                      // <MinorSpokes> //
-                      _buildMinorSpoke(326.25),
-                      _buildMinorSpoke(337.5),
-                      _buildMinorSpoke(348.75),
-                      // </MinorSpokes> //
-                    ],
+                        // <MinorSpokes> //
+                        _buildMinorSpoke(326.25),
+                        _buildMinorSpoke(337.5),
+                        _buildMinorSpoke(348.75),
+                        // </MinorSpokes> //
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
-    );
+
+      // Ticker
+      Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: Neumorphic(
+            style: NeumorphicStyle(
+                depth: -20, lightSource: LightSource.topRight, intensity: 0.75),
+            child: Container(
+              width: 3,
+              height: 36,
+              decoration: BoxDecoration(color: Colors.red[900]),
+            ),
+          )),
+
+      // Ticker Handle
+      Padding(
+          padding: EdgeInsets.only(top: 50),
+          child: Neumorphic(
+            style: NeumorphicStyle(
+                depth: -20, lightSource: LightSource.topRight, intensity: 0.75),
+            child: Container(
+              width: 7,
+              height: 7,
+              decoration: BoxDecoration(color: Colors.red[900]),
+            ),
+          )),
+    ]);
   }
 
   Widget _buildMajorSpoke(
