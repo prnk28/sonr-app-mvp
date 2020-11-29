f9c6711b2bcadc80e41c3d6b26aae47488205d54
Prad <prnk28@gmail.com>
2020-11-29 03:52:15 -0500

    Create observable bubble

--- lib/ui/pages/transfer/widgets/bubble.dart
+++ lib/ui/pages/transfer/widgets/bubble.dart
@@ -11,91 +11,63 @@ class Bubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ^ Looking for Peer ^ //
-    if (sonr.status() == SonrStatus.Searching) {
-      // @ Check if Ive already Declined
-      if (sonr.auth() != null) {
-        // Check if im the peer
+    return Obx(() {
+      if (sonr.status() == SonrStatus.Searching) {
+        // @ Check if Ive already Declined
+        if (sonr.auth() != null) {
+          // Check if im the peer
+          if (_isCurrentPeer()) {
+            return Positioned(
+                top: calculateOffset(value).dy,
+                left: calculateOffset(value).dx,
+                child: Container(
+                    child: Neumorphic(
+                  style: NeumorphicStyle(
+                      shape: NeumorphicShape.flat,
+                      boxShape: NeumorphicBoxShape.circle(),
+                      depth: 10,
+                      lightSource: LightSource.topLeft,
+                      color: Colors.grey[300]),
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    child: FlareActor("assets/animations/denied.flr",
+                        alignment: Alignment.center,
+                        fit: BoxFit.contain,
+                        animation: "animate"),
+                  ),
+                )));
+          }
+        }
+      }
+      // ^ Check if im the Peer ^ //
+      else if (sonr.status() == SonrStatus.Pending) {
+        if (_isCurrentPeer()) {
+          return PendingBubble(value, peer);
+        }
+      }
+
+      // ^ Check if User is Transferring ^ //
+      else if (sonr.status() == SonrStatus.Transferring) {
         if (_isCurrentPeer()) {
           return Positioned(
               top: calculateOffset(value).dy,
               left: calculateOffset(value).dx,
-              child: Container(
-                  child: Neumorphic(
-                style: NeumorphicStyle(
-                    shape: NeumorphicShape.flat,
-                    boxShape: NeumorphicBoxShape.circle(),
-                    depth: 10,
-                    lightSource: LightSource.topLeft,
-                    color: Colors.grey[300]),
-                child: Container(
-                  width: 80,
-                  height: 80,
-                  child: FlareActor("assets/animations/denied.flr",
-                      alignment: Alignment.center,
-                      fit: BoxFit.contain,
-                      animation: "animate"),
-                ),
-              )));
-        }
-      } else {
-        // ^ Active Peer you can invite me ^
-        return Positioned(
-            top: calculateOffset(value).dy,
-            left: calculateOffset(value).dx,
-            child: GestureDetector(
-                onTap: () async {
-                  // Send Offer to Bubble
-                  sonr.invitePeer(peer);
-                },
-                child: Neumorphic(
+              child: Stack(alignment: AlignmentDirectional.center, children: [
+                Neumorphic(
                     style: NeumorphicStyle(
-                        shape: NeumorphicShape.flat,
-                        boxShape: NeumorphicBoxShape.circle(),
-                        depth: 10,
-                        lightSource: LightSource.topLeft,
-                        color: Colors.grey[300]),
-                    child: Container(
-                      width: 80,
-                      height: 80,
-                      child: Column(
-                        children: [
-                          Spacer(),
-                          initialsFromPeer(peer),
-                          iconFromPeer(peer),
-                        ],
-                      ),
-                    ))));
+                        depth: 10, boxShape: NeumorphicBoxShape.circle()),
+                    child: SizedBox(
+                      child: CircularProgressIndicator(strokeWidth: 6),
+                      height: 86.0,
+                      width: 86.0,
+                    )),
+                buildBubbleContent(peer),
+              ]));
+        }
       }
-    }
-    // ^ Check if im the Peer ^ //
-    else if (sonr.status() == SonrStatus.Pending) {
-      if (_isCurrentPeer()) {
-        return PendingBubble(value, peer);
-      }
-    }
 
-    // ^ Check if User is Transferring ^ //
-    else if (sonr.status() == SonrStatus.Transferring) {
-      if (_isCurrentPeer()) {
-        return Positioned(
-            top: calculateOffset(value).dy,
-            left: calculateOffset(value).dx,
-            child: Stack(alignment: AlignmentDirectional.center, children: [
-              Neumorphic(
-                  style: NeumorphicStyle(
-                      depth: 10, boxShape: NeumorphicBoxShape.circle()),
-                  child: SizedBox(
-                    child: CircularProgressIndicator(strokeWidth: 6),
-                    height: 86.0,
-                    width: 86.0,
-                  )),
-              buildBubbleContent(peer),
-            ]));
-      }
-    }
-
-    // ^ Active Peer you can invite me ^
-    else {
+      // ^ Active Peer you can invite me ^
       return Positioned(
           top: calculateOffset(value).dy,
           left: calculateOffset(value).dx,
@@ -122,7 +94,7 @@ class Bubble extends StatelessWidget {
                       ],
                     ),
                   ))));
-    }
+    });
   }
 
   // ** Peer Checking Method ** //
