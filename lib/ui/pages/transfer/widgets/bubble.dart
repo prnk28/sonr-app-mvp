c61de5cc3e83222635c79a163264a3070777d6cf
Prad <prnk28@gmail.com>
2020-12-07 21:51:40 -0500

    Updated files on 2020-12-07 11:15:14

--- lib/ui/pages/transfer/widgets/bubble.dart
+++ lib/ui/pages/transfer/widgets/bubble.dart
@@ -30,64 +30,72 @@ class Bubble extends StatelessWidget {
                   transferController.invitePeer(peer);
                 }
               },
-              child: Container(
-                  width: 90,
-                  height: 90,
-                  decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(167, 179, 190, 1.0),
-                      offset: Offset(0, 2),
-                      blurRadius: 6,
-                      spreadRadius: 0.5,
-                    ),
-                    BoxShadow(
-                      color: Color.fromRGBO(248, 252, 255, .5),
-                      offset: Offset(-2, 0),
-                      blurRadius: 6,
-                      spreadRadius: 0.5,
-                    ),
-                  ]),
-                  child: Stack(alignment: Alignment.center, children: [
-                    Rive(
-                      artboard: bubbleController.artboard,
-                      alignment: Alignment.center,
-                      fit: BoxFit.contain,
-                    ),
-                    GetBuilder<BubbleAnimController>(builder: (_) {
-                      if (bubbleController.hasCompleted()) {
-                        return PlayAnimation<double>(
-                            tween: (1.0).tweenTo(0.0),
-                            duration: 20.milliseconds,
-                            builder: (context, child, value) {
-                              return AnimatedOpacity(
-                                  opacity: value,
+              child: PlayAnimation<double>(
+                  tween: (0.0).tweenTo(1.0),
+                  duration: 500.milliseconds,
+                  delay: 1.seconds,
+                  builder: (context, child, value) {
+                    return Container(
+                        width: 90,
+                        height: 90,
+                        decoration:
+                            BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                          BoxShadow(
+                            color: Color.fromRGBO(167, 179, 190, value),
+                            offset: Offset(0, 2),
+                            blurRadius: 6,
+                            spreadRadius: 0.5,
+                          ),
+                          BoxShadow(
+                            color: Color.fromRGBO(248, 252, 255, value / 2),
+                            offset: Offset(-2, 0),
+                            blurRadius: 6,
+                            spreadRadius: 0.5,
+                          ),
+                        ]),
+                        child: Stack(alignment: Alignment.center, children: [
+                          Rive(
+                            artboard: bubbleController.artboard,
+                            alignment: Alignment.center,
+                            fit: BoxFit.contain,
+                          ),
+                          GetBuilder<BubbleAnimController>(builder: (_) {
+                            if (bubbleController.hasCompleted()) {
+                              return PlayAnimation<double>(
+                                  tween: (1.0).tweenTo(0.0),
                                   duration: 20.milliseconds,
-                                  child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        iconFromPeer(peer, size: 20),
-                                        initialsFromPeer(peer),
-                                      ]));
-                            });
-                      }
-                      return PlayAnimation<double>(
-                          tween: (0.0).tweenTo(1.0),
-                          duration: 500.milliseconds,
-                          delay: 1.seconds,
-                          builder: (context, child, value) {
-                            return AnimatedOpacity(
-                                opacity: value,
+                                  builder: (context, child, value) {
+                                    return AnimatedOpacity(
+                                        opacity: value,
+                                        duration: 20.milliseconds,
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              iconFromPeer(peer, size: 20),
+                                              initialsFromPeer(peer),
+                                            ]));
+                                  });
+                            }
+                            return PlayAnimation<double>(
+                                tween: (0.0).tweenTo(1.0),
                                 duration: 500.milliseconds,
-                                child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      iconFromPeer(peer, size: 20),
-                                      initialsFromPeer(peer),
-                                    ]));
-                          });
-                    }),
-                  ]))));
+                                delay: 1.seconds,
+                                builder: (context, child, value) {
+                                  return AnimatedOpacity(
+                                      opacity: value,
+                                      duration: 500.milliseconds,
+                                      child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            iconFromPeer(peer, size: 20),
+                                            initialsFromPeer(peer),
+                                          ]));
+                                });
+                          }),
+                        ]));
+                  })));
     });
   }
 }
