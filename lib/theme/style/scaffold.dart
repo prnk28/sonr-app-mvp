df1dc6acdcc6c98ba02e0f4c62d4bc93d6bf4f6a
Prad <prnk28@gmail.com>
2021-04-24 06:40:53 -0400

    Updated files on 2021-04-23 13:38:00

--- lib/theme/style/scaffold.dart
+++ lib/theme/style/scaffold.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:sonr_app/theme/theme.dart';
+import 'dart:math';
 
 // ^ Standardized Uniform Scaffold ^ //
 class SonrScaffold extends StatelessWidget {
@@ -28,6 +29,7 @@ class SonrScaffold extends StatelessWidget {
           extendBodyBehindAppBar: true,
           resizeToAvoidBottomInset: false,
           backgroundColor: UserService.isDarkMode ? SonrColor.Black.withOpacity(0.75) : SonrColor.White.withOpacity(0.75),
+          floatingActionButtonLocation: _FixedCenterDockedFabLocation(),
           body: Stack(
             children: [
               // Gradient
@@ -51,17 +53,43 @@ class SonrScaffold extends StatelessWidget {
                 filter: ImageFilter.blur(sigmaX: 5.1188, sigmaY: 5.1188),
                 child: Container(width: Get.width, height: Get.height),
               ),
-              SafeArea(child: body),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: DeviceService.keyboardVisible.value ? Container() : floatingAction,
-              )
+              SafeArea(child: body)
             ],
           ),
           appBar: appBar,
           bottomNavigationBar: bottomNavigationBar,
+          floatingActionButton: DeviceService.keyboardVisible.value ? Container() : floatingAction,
           bottomSheet: bottomSheet,
         ));
+  }
+}
+
+// ^ Fixed Location for Center Docked ^ //
+class _FixedCenterDockedFabLocation extends FloatingActionButtonLocation {
+  const _FixedCenterDockedFabLocation();
+
+  @protected
+  double getDockedY(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    final double contentBottom = scaffoldGeometry.contentBottom;
+    final double bottomSheetHeight = scaffoldGeometry.bottomSheetSize.height;
+    final double fabHeight = scaffoldGeometry.floatingActionButtonSize.height;
+    final double snackBarHeight = scaffoldGeometry.snackBarSize.height;
+    double bottomDistance = MediaQuery.of(Get.context).viewInsets.bottom;
+    double fabY = contentBottom + bottomDistance - fabHeight / 2.0;
+
+    // The FAB should sit with a margin between it and the snack bar.
+    if (snackBarHeight > 0.0) fabY = min(fabY, contentBottom - snackBarHeight - fabHeight - kFloatingActionButtonMargin);
+    // The FAB should sit with its center in front of the top of the bottom sheet.
+    if (bottomSheetHeight > 0.0) fabY = min(fabY, contentBottom - bottomSheetHeight - fabHeight / 2.0);
+
+    final double maxFabY = scaffoldGeometry.scaffoldSize.height - fabHeight;
+    return min(maxFabY, fabY);
+  }
+
+  @override
+  Offset getOffset(ScaffoldPrelayoutGeometry scaffoldGeometry) {
+    final double fabX = (scaffoldGeometry.scaffoldSize.width - scaffoldGeometry.floatingActionButtonSize.width) / 2.0;
+    return Offset(fabX, getDockedY(scaffoldGeometry));
   }
 }
 
