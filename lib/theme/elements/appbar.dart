93cb67b828afd07451ff0b07f868c5c9dc459cd9
Prad <prnk28@gmail.com>
2021-04-23 20:52:15 -0400

    Updated files on 2021-04-23 13:38:00

--- lib/theme/elements/appbar.dart
+++ lib/theme/elements/appbar.dart
@@ -4,17 +4,23 @@ import '../theme.dart';
 
 class DesignAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget title;
+  final Widget subtitle;
   final Widget leading;
   final Widget action;
-  DesignAppBar({this.title, this.leading, this.action});
+  DesignAppBar({@required this.title, this.leading, this.action, this.subtitle});
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 14.0, right: 14, top: 28.0),
       child: NavigationToolbar(
         leading: leading,
-        trailing: action,
-        middle: title,
+        trailing: action != null ? action : Container(width: 56, height: 56),
+        middle: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [subtitle ?? subtitle, title],
+        ),
         centerMiddle: false,
       ),
     );
