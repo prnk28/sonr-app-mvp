b70a155885c545be4c7bc824fd9c32a262204759
Prad <prnk28@gmail.com>
2021-04-27 16:51:55 -0400

    Convert Chart Storage widget to be OBX

--- lib/views/home/main/storage_chart.dart
+++ lib/views/home/main/storage_chart.dart
@@ -1,106 +1,33 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
 import 'package:sonr_app/theme/theme.dart';
+import 'grid_controller.dart';
 
-/// Icons by svgrepo.com (https://www.svgrepo.com/collection/job-and-professions-3/)
-class StorageChart extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => StorageChartState();
-}
-
-class StorageChartState extends State {
-  int touchedIndex;
-
+class StorageChart extends GetView<GridController> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PieChart(
-        PieChartData(
-            pieTouchData: PieTouchData(touchCallback: (pieTouchResponse) {
-              setState(() {
-                final desiredTouch = pieTouchResponse.touchInput is! PointerExitEvent && pieTouchResponse.touchInput is! PointerUpEvent;
-                if (desiredTouch && pieTouchResponse.touchedSection != null) {
-                  touchedIndex = pieTouchResponse.touchedSection.touchedSectionIndex;
-                } else {
-                  touchedIndex = -1;
-                }
-              });
-            }),
-            borderData: FlBorderData(
-              show: false,
-            ),
-            sectionsSpace: 0,
-            centerSpaceRadius: 0,
-            sections: showingSections()),
-      ),
-    );
-  }
-
-  List<PieChartSectionData> showingSections() {
-    return List.generate(3, (i) {
-      final isTouched = i == touchedIndex;
-      final double fontSize = isTouched ? 16 : 12;
-      final double radius = isTouched ? 72 : 58;
-      final double widgetSize = isTouched ? 36 : 28;
-
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            color: SonrColor.Secondary,
-            value: 50,
-            title: '50%',
-            radius: radius,
-            titleStyle: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-            badgeWidget: _Badge(
-              size: widgetSize,
-              borderColor: SonrColor.Secondary,
-              child: SonrIcons.Video.gradient(gradient: SonrGradient.Primary, size: 18),
-            ),
-            badgePositionPercentageOffset: .96,
-          );
-        case 1:
-          return PieChartSectionData(
-            color: SonrColor.Tertiary,
-            value: 34,
-            title: '34%',
-            radius: radius,
-            titleStyle: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-            badgeWidget: _Badge(
-              size: widgetSize,
-              borderColor: SonrColor.Tertiary,
-              child: SonrIcons.Files.gradient(gradient: SonrGradient.Tertiary, size: 18),
-            ),
-            badgePositionPercentageOffset: 1.02,
-          );
-        case 2:
-          return PieChartSectionData(
-            color: SonrColor.AccentPink,
-            value: 16,
-            title: '16%',
-            radius: radius,
-            titleStyle: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold, color: const Color(0xffffffff)),
-            badgeWidget: _Badge(
-              size: widgetSize,
-              borderColor: SonrColor.AccentPink,
-              child: SonrIcons.Photos.gradient(gradient: SonrGradient.Critical, size: 18),
-            ),
-            badgePositionPercentageOffset: .96,
-          );
-        default:
-          return null;
-      }
-    });
+    return Obx(() => Container(
+          child: PieChart(
+            PieChartData(
+                pieTouchData: PieTouchData(touchCallback: (pieTouchResponse) => controller.tapChart(pieTouchResponse)),
+                borderData: FlBorderData(
+                  show: false,
+                ),
+                sectionsSpace: 0,
+                centerSpaceRadius: 0,
+                sections: controller.chartData),
+          ),
+        ));
   }
 }
 
-class _Badge extends StatelessWidget {
+class Badge extends StatelessWidget {
   final double size;
   final Color borderColor;
   final Widget child;
 
-  const _Badge({
+  const Badge({
     Key key,
     @required this.size,
     @required this.borderColor,
