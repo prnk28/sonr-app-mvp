b1a2f27a2afac76e3f8f3bb1492e968ddee68689
Prad <prnk28@gmail.com>
2021-04-28 23:13:14 -0400

    Updated files on 2021-04-28 23:08:33

--- go/cmd/options.go
+++ go/cmd/options.go
@@ -4,11 +4,13 @@ import (
 	flutter_systray "github.com/JanezStupar/flutter_systray/go"
 	"github.com/go-flutter-desktop/go-flutter"
 	"github.com/go-flutter-desktop/plugins/path_provider"
+	warble "github.com/jslater89/warble/go"
 )
 
 var options = []flutter.Option{
 	flutter.WindowInitialDimensions(1280, 800),
 	flutter.AddPlugin(&flutter_systray.FlutterSystrayPlugin{}),
+	flutter.AddPlugin(warble.New()),
 	flutter.AddPlugin(&path_provider.PathProviderPlugin{
 		VendorName:      "https://sonr.io",
 		ApplicationName: "Sonr",
