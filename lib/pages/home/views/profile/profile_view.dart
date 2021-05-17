4e8cbf527f477ae2ed748311718dd9b9addf66dc
Prad <prnk28@gmail.com>
2021-05-17 03:08:48 -0400

    Updated files on 2021-05-16 18:14:57

--- lib/pages/home/views/profile/profile_view.dart
+++ lib/pages/home/views/profile/profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:sonr_app/modules/card/tile/tile_item.dart';
+import 'package:sonr_app/modules/search/social_search.dart';
 import 'package:sonr_app/style/style.dart';
 import 'profile_controller.dart';
 
@@ -79,7 +80,7 @@ class _ProfileHeaderBar extends GetView<ProfileController> {
       snap: false,
       backgroundColor: Colors.transparent,
       foregroundColor: Colors.transparent,
-      leading: PlainIconButton(icon: SonrIcons.Plus.gradient(value: SonrGradient.Secondary), onPressed: controller.setAddTile),
+      leading: PlainIconButton(icon: SonrIcons.Plus.gradient(value: SonrGradient.Primary), onPressed: controller.setAddTile),
       actions: [PlainIconButton(icon: SonrIcons.Edit.gradient(value: SonrGradient.Tertiary), onPressed: controller.setEditingMode)],
       expandedHeight: Get.height / 6 + 16,
       flexibleSpace: FlexibleSpaceBar(
@@ -108,7 +109,7 @@ class _ProfileInfoView extends GetView<ProfileController> {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 14),
       width: Get.width,
-      height: 175,
+      height: 200,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -117,16 +118,20 @@ class _ProfileInfoView extends GetView<ProfileController> {
           ["${UserService.contact.value.username}".h3, ".snr/".h3_Grey].row(),
 
           // First/Last Name
-          GestureDetector(
-              onLongPress: controller.setEditingMode,
-              child: Obx(() => "${UserService.contact.value.firstName} ${UserService.contact.value.lastName}".h5)),
-          Padding(padding: EdgeInsets.all(8)),
+          _buildName(),
+          Padding(padding: EdgeInsets.all(12)),
           // Bio/ LastTweet
           _buildBio(),
           _buildLastTweet(),
         ],
       ),
     );
+  }
+
+  Widget _buildName() {
+    return GestureDetector(
+        onLongPress: controller.setEditingMode,
+        child: Obx(() => [UserService.contact.value.fullName.h5, _ProfileContactButtons()].row(mainAxisAlignment: MainAxisAlignment.spaceBetween)));
   }
 
   Widget _buildBio() {
@@ -138,6 +143,45 @@ class _ProfileInfoView extends GetView<ProfileController> {
   }
 
   Widget _buildLastTweet() {
-    return Container(width: Get.width, height: 72, decoration: Neumorphic.indented(theme: Get.theme));
+    return ObxValue<RxBool>((isLinkingTwitter) {
+      if (isLinkingTwitter.value) {
+        return SocialUserSearchField.twitter(value: "");
+      } else {
+        return Container(
+          width: Get.width,
+          height: 72,
+          decoration: Neumorphic.indented(theme: Get.theme),
+          child: UserService.contact.value.hasSocial(Contact_Social_Provider.Twitter)
+              ? Text("Last Tweet")
+              : GestureDetector(
+                  onTap: () => isLinkingTwitter(true),
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [SonrIcons.Twitter.gradient(size: 32), Padding(padding: EdgeInsets.all(8)), "Tap to Link Twitter".h6],
+                    ),
+                  ),
+                ),
+        );
+      }
+    }, false.obs);
+  }
+}
+
+class _ProfileContactButtons extends GetView<ProfileController> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(children: [
+        PlainIconButton(onPressed: () {}, icon: SonrIcons.Call.gradient(value: SonrGradient.Secondary, size: 22)),
+        Padding(padding: EdgeInsets.only(right: 4)),
+        PlainIconButton(onPressed: () {}, icon: SonrIcons.Message.gradient(value: SonrGradient.Secondary, size: 22)),
+        Padding(padding: EdgeInsets.only(right: 4)),
+        PlainIconButton(onPressed: () {}, icon: SonrIcons.Video.gradient(value: SonrGradient.Secondary, size: 22)),
+        Padding(padding: EdgeInsets.only(right: 4)),
+        PlainIconButton(onPressed: () {}, icon: SonrIcons.ATSign.gradient(value: SonrGradient.Secondary, size: 22)),
+      ]),
+    );
   }
 }
