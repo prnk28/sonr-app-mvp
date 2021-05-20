07d61edde4f361b0f9ad98bee6157ae5879dc6c7
Prad <prnk28@gmail.com>
2021-05-20 01:52:02 -0400

    Updated files on 2021-05-19 21:52:47

--- lib/data/model/rx_proto.dart
+++ lib/data/model/rx_proto.dart
@@ -64,22 +64,30 @@ extension RxContact on Rx<Contact> {
 
   /// Set FirstName for Rx<Contact>
   void setFirstName(String data) => this.update((val) {
-        val?.setFirstName(data);
+        if (val != null) {
+          val.setFirstName(data);
+        }
       });
 
   /// Set LastName for Rx<Contact>
   void setLastName(String data) => this.update((val) {
-        val?.setLastName(data);
+        if (val != null) {
+          val.setLastName(data);
+        }
       });
 
   /// Set Picture for Rx<Contact>
   void setPicture(Uint8List data) => this.update((val) {
-        val?.setPicture(data);
+        if (val != null) {
+          val.setPicture(data);
+        }
       });
 
   /// Delete a Social Media Provider
   void updateSocial(Contact_Social data) => this.update((val) {
-        val?.updateSocial(data);
+        if (val != null) {
+          val.updateSocial(data);
+        }
       });
 }
 
