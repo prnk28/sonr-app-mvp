0545016369962e0d33a10ed67925191d579b4934
Prad <prnk28@gmail.com>
2021-05-26 03:12:51 -0700

    Updated files on 2021-05-24 19:57:30

--- lib/pages/home/views/contact/fields/name_field.dart
+++ lib/pages/home/views/contact/fields/name_field.dart
@@ -4,17 +4,17 @@ import '../profile_controller.dart';
 /// @ Edit Profile Details View
 class EditNameView extends GetView<ProfileController> {
   EditNameView({Key? key}) : super(key: key);
-  final FocusNode _prefixNameFocus = FocusNode();
+  // TODO: final FocusNode _prefixNameFocus = FocusNode();
   final FocusNode _firstNameFocus = FocusNode();
-  final FocusNode _middleNameFocus = FocusNode();
+  // TODO: final FocusNode _middleNameFocus = FocusNode();
   final FocusNode _lastNameFocus = FocusNode();
-  final FocusNode _suffixNameFocus = FocusNode();
-  final FocusNode _nickNameFocus = FocusNode();
+  // TODO: final FocusNode _suffixNameFocus = FocusNode();
+  // TODO: final FocusNode _nickNameFocus = FocusNode();
   final hintName = SonrTextField.hintName();
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(shrinkWrap: true, primary: true, slivers: [
+    return CustomScrollView(primary: true, slivers: [
       // @ Top Banner
       SliverToBoxAdapter(
         child: Container(
@@ -30,72 +30,81 @@ class EditNameView extends GetView<ProfileController> {
               ]),
         ),
       ),
+      // TODO:
       SliverList(
           delegate: SliverChildListDelegate.fixed([
-        SonrTextField(
-            hint: "Dr.",
-            label: "Prefix",
-            autoFocus: true,
-            focusNode: _prefixNameFocus,
-            textInputAction: TextInputAction.next,
-            onEditingComplete: () {
-              _firstNameFocus.requestFocus();
-            },
-            value: UserService.contact.value.general.name.prefix,
-            onChanged: (val) => controller.editedFirstName(val)),
+        // SonrTextField(
+        //     hint: "Dr.",
+        //     label: "Prefix",
+        //     autoFocus: true,
+        //     focusNode: _prefixNameFocus,
+        //     textInputAction: TextInputAction.next,
+        //     controller: TextEditingController(text: UserService.contact.value.general.name.prefix),
+        //     onEditingComplete: () {
+        //       _firstNameFocus.requestFocus();
+        //     },
+        //     value: controller.editedFirstName.value,
+        //     onChanged: (val) => controller.editedFirstName(val)),
         SonrTextField(
             hint: hintName.item1,
             label: "First Name",
             focusNode: _firstNameFocus,
             textInputAction: TextInputAction.next,
-            onEditingComplete: () {
-              _middleNameFocus.requestFocus();
-            },
-            value: controller.editedFirstName.value,
-            onChanged: (val) => controller.editedFirstName(val)),
-        SonrTextField(
-            hint: "A.",
-            label: "Middle Name",
-            focusNode: _middleNameFocus,
-            textInputAction: TextInputAction.next,
+            controller: TextEditingController(text: UserService.contact.value.firstName),
             onEditingComplete: () {
               _lastNameFocus.requestFocus();
             },
-            value: UserService.contact.value.general.name.middleName,
+            value: controller.editedFirstName.value,
             onChanged: (val) => controller.editedFirstName(val)),
+        // TODO:
+        // SonrTextField(
+        //     hint: "A.",
+        //     label: "Middle Name",
+        //     focusNode: _middleNameFocus,
+        //     textInputAction: TextInputAction.next,
+        //     controller: TextEditingController(text: UserService.contact.value.general.name.middleName),
+        //     onEditingComplete: () {
+        //       _lastNameFocus.requestFocus();
+        //     },
+        //     value: controller.editedFirstName.value,
+        //     onChanged: (val) => controller.editedFirstName(val)),
         SonrTextField(
             hint: hintName.item2,
             label: "Last Name",
             textInputAction: TextInputAction.next,
+            controller: TextEditingController(text: UserService.contact.value.lastName),
             focusNode: _lastNameFocus,
             value: controller.editedLastName.value,
             onEditingComplete: () {
-              _suffixNameFocus.requestFocus();
-            },
-            onChanged: (val) => controller.editedLastName(val)),
-        SonrTextField(
-            hint: "Jr.",
-            label: "Suffix",
-            textInputAction: TextInputAction.done,
-            value: UserService.contact.value.general.name.suffix,
-            focusNode: _suffixNameFocus,
-            onEditingComplete: () {
-              controller.saveEditedDetails();
-              _nickNameFocus.requestFocus();
-            },
-            onChanged: (val) => controller.editedPhone(val)),
-        SonrTextField(
-            hint: "Rocket",
-            label: "Nick Name",
-            textInputAction: TextInputAction.next,
-            controller: TextEditingController(text: UserService.contact.value.lastName),
-            focusNode: _nickNameFocus,
-            value: UserService.contact.value.general.name.nickName,
-            onEditingComplete: () {
-              _nickNameFocus.unfocus();
+              _lastNameFocus.unfocus();
               controller.saveEditedDetails();
             },
             onChanged: (val) => controller.editedLastName(val)),
+        // TODO:
+        // SonrTextField(
+        //     hint: "Jr.",
+        //     label: "Suffix",
+        //     textInputAction: TextInputAction.done,
+        //     controller: TextEditingController(text: UserService.contact.value.hasPhone() ? UserService.contact.value.phonePrimary : ""),
+        //     value: controller.editedLastName.value,
+        //     focusNode: _suffixNameFocus,
+        //     onEditingComplete: () {
+        //       controller.saveEditedDetails();
+        //       _nickNameFocus.requestFocus();
+        //     },
+        //     onChanged: (val) => controller.editedPhone(val)),
+        // SonrTextField(
+        //     hint: "Rocket",
+        //     label: "Nick Name",
+        //     textInputAction: TextInputAction.next,
+        //     controller: TextEditingController(text: UserService.contact.value.lastName),
+        //     focusNode: _nickNameFocus,
+        //     value: controller.editedLastName.value,
+        //     onEditingComplete: () {
+        //       _nickNameFocus.unfocus();
+        //       controller.saveEditedDetails();
+        //     },
+        //     onChanged: (val) => controller.editedLastName(val)),
       ])),
     ]);
   }
