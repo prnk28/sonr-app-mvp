f9536fde19c1a7f9dfbfcf9804c8c8dfbdf03e91
Prad <prnk28@gmail.com>
2021-05-30 00:14:55 -0700

    Updated files on 2021-05-29 18:21:55

--- lib/service/device/device.dart
+++ lib/service/device/device.dart
@@ -5,6 +5,8 @@ import 'mobile.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:platform_device_id/platform_device_id.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:geolocator/geolocator.dart';
 
 class DeviceService extends GetxService {
   // Accessors
@@ -62,7 +64,7 @@ class DeviceService extends GetxService {
     });
 
     // Set Location
-    _location(await findIPLocation());
+    _location(await findLocation(platform));
     return this;
   }
 
@@ -74,7 +76,21 @@ class DeviceService extends GetxService {
   }
 
   /// @ Retreive Location by IP Address
-  static Future<Location> findIPLocation() async {
+  static Future<Location> findLocation(Platform platform) async {
+    // # Check Platform
+    if (platform.isMobile) {
+      // Find Connectivity
+      Connectivity _connectivity = Connectivity();
+      var result = await _connectivity.checkConnectivity();
+
+      // Check for Mobile Data
+      if (result == ConnectivityResult.mobile) {
+        var pos = await Geolocator.getCurrentPosition();
+        return Location(longitude: pos.longitude, latitude: pos.latitude);
+      }
+    }
+
+    // # IP Based Location for ALL Devices
     var url = Uri.parse("https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=${Env.ip_key}");
 
     final response = await http.get(url, headers: {'x-rapidapi-key': Env.rapid_key, 'x-rapidapi-host': Env.rapid_host});
