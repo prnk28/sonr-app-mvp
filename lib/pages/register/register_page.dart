df109a92499b2efe59253a8b6c563fdfb05705f2
Prad <prnk28@gmail.com>
2021-06-10 21:11:05 -0700

    Updated files on 2021-06-10 20:53:35

--- lib/pages/register/register_page.dart
+++ lib/pages/register/register_page.dart
@@ -9,111 +9,11 @@ class RegisterPage extends GetView<RegisterController> {
       width: Get.width,
       height: Get.height,
       decoration: Neumorphic.floating(theme: Get.theme),
-      child: DeviceService.isMobile ? _RegisterMobile() : _RegisterDesktop(),
-    );
-  }
-}
-
-class _RegisterDesktop extends GetView<RegisterController> {
-  final RxInt counter = 0.obs;
-  final hintName = SonrTextField.hintName();
-  final lastNameFocus = FocusNode();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Obx(
-      () => Container(
-        padding: EdgeInsets.symmetric(horizontal: 280),
-        child: Column(children: <Widget>[
-          Container(
-            // child: SonrAssetLogo.Side.widget,
-            height: 128,
-          ),
-          VerticalDivider(),
-          Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // ****************** //
-                // ** <First Name> ** //
-                // ****************** //
-                SonrTextField(
-                    label: "First Name",
-                    hint: hintName.item1,
-                    value: controller.firstName.value,
-                    status: controller.firstNameStatus,
-                    textInputAction: TextInputAction.next,
-                    textCapitalization: TextCapitalization.words,
-                    autoFocus: true,
-                    autoCorrect: false,
-                    onEditingComplete: () {
-                      FocusScope.of(context).requestFocus(lastNameFocus);
-                      controller.firstName(controller.firstName.value.capitalizeFirst!);
-                      controller.firstName.refresh();
-                    },
-                    onChanged: (String value) {
-                      controller.firstName(value);
-                      controller.firstName.refresh();
-                    }),
-
-                // ***************** //
-                // ** <Last Name> ** //
-                // ***************** //
-                SonrTextField(
-                    label: "Last Name",
-                    hint: hintName.item2,
-                    textInputAction: TextInputAction.next,
-                    value: controller.lastName.value,
-                    textCapitalization: TextCapitalization.words,
-                    focusNode: lastNameFocus,
-                    status: controller.lastNameStatus,
-                    autoCorrect: false,
-                    onEditingComplete: () {
-                      controller.lastName(controller.lastName.value.capitalizeFirst!);
-                      controller.lastName.refresh();
-                      FocusScopeNode currentFocus = FocusScope.of(Get.context!);
-                      if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
-                        FocusManager.instance.primaryFocus!.unfocus();
-                        controller.setContact();
-                      }
-                    },
-                    onChanged: (String value) {
-                      controller.lastName(value);
-                      controller.lastName.refresh();
-                    }),
-
-                // ********************* //
-                // ** <Submit Button> ** //
-                // ********************* //
-                Center(
-                  child: Container(
-                    padding: EdgeInsets.only(top: 48.0, left: 200, right: 200),
-                    child: ColorButton.primary(
-                      icon: SonrIcons.Link,
-                      text: "Link",
-                      onPressed: () {
-                        controller.setContact();
-                      },
-                      //margin: EdgeInsets.only(top: 12),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          )
-        ]),
-      )),
-    );
-  }
-}
-
-class _RegisterMobile extends GetView<RegisterController> {
-  @override
-  Widget build(BuildContext context) {
-    return Obx(
-      () => AnimatedSlideSwitcher.slideRight(
-        child: _buildView(controller.status.value),
-        duration: const Duration(milliseconds: 2500),
+      child: Obx(
+        () => AnimatedSlideSwitcher.slideRight(
+          child: _buildView(controller.status.value),
+          duration: const Duration(milliseconds: 2500),
+        ),
       ),
     );
   }
--- lib/pages/register/views/views.dart
+++ lib/pages/register/views/views.dart
@@ -1,6 +1,6 @@
 export 'backup_view.dart';
 export 'contact_view.dart';
-export 'link_view.dart';
+
 export 'name_view.dart';
 export 'permissions_view.dart';
 export 'restore_view.dart';
