db6672f79cfabe427f41b1e2a7783c2791824feb
Prad <prnk28@gmail.com>
2021-06-18 14:58:24 -0700

    Updated files on 2021-06-18 12:23:01

--- lib/data/core/arguments.dart
+++ lib/data/core/arguments.dart
@@ -3,7 +3,6 @@ import 'package:sonr_app/service/client/sonr.dart';
 import 'package:sonr_app/style.dart';
 import 'package:sonr_app/data/data.dart';
 
-
 // ** ─── BuildMode Arguments ────────────────────────────────────────────────────────
 /// Build Mode Enum
 enum BuildMode { Release, Debug }
@@ -35,22 +34,34 @@ class SnackArgs {
   final Widget icon;
   final int duration;
   final bool shouldIconPulse;
+  final SnackPosition position;
 
-  SnackArgs(this.title, this.message, this.icon, this.color, this.duration, this.shouldIconPulse);
+  SnackArgs(this.title, this.message, this.icon, this.color, this.duration, this.shouldIconPulse, this.position);
 
   /// @ Custom Alert
-  factory SnackArgs.remote({required String message, int duration = 45000, Color color = Colors.purple}) {
-    return SnackArgs("Remote Code", message, SonrIcons.Remote.white, color, duration, true);
+  factory SnackArgs.remote({
+    required String message,
+    int duration = 45000,
+    Color color = Colors.purple,
+    SnackPosition position = SnackPosition.BOTTOM,
+  }) {
+    return SnackArgs("Remote Code", message, SonrIcons.Remote.white, color, duration, true, position);
   }
 
   /// @ Custom Alert
-  factory SnackArgs.alert({required String title, required String message, required Icon icon, Color color = Colors.orange}) {
-    return SnackArgs(title, message, icon, color, 2600, false);
+  factory SnackArgs.alert({
+    required String title,
+    required String message,
+    required Icon icon,
+    Color color = Colors.orange,
+    SnackPosition position = SnackPosition.BOTTOM,
+  }) {
+    return SnackArgs(title, message, icon, color, 2600, false, position);
   }
 
   /// @ Cancelled Operation
-  factory SnackArgs.cancelled(String message) {
-    return SnackArgs("Cancelled.", message, SonrIcons.Stop.white, Colors.yellow, 2600, false);
+  factory SnackArgs.cancelled(String message, {SnackPosition position = SnackPosition.BOTTOM}) {
+    return SnackArgs("Cancelled.", message, SonrIcons.Stop.white, Colors.yellow, 2600, false, position);
   }
 
   /// @ Error on Operation
@@ -61,40 +72,89 @@ class SnackArgs {
         // Orange - Title Failed
         case ErrorMessage_Severity.CRITICAL:
           DeviceService.playSound(type: UISoundType.Critical);
-          return SnackArgs("Failed", error.message, Icon(Icons.sms_failed_outlined), Colors.orange, 2600, false);
+          return SnackArgs(
+            "Failed",
+            error.message,
+            Icon(Icons.sms_failed_outlined),
+            Colors.orange,
+            2600,
+            false,
+            SnackPosition.TOP,
+          );
 
         // Red - Title Error
         case ErrorMessage_Severity.FATAL:
           DeviceService.playSound(type: UISoundType.Fatal);
-          return SnackArgs("Error", error.message, SonrIcons.Caution.white, Colors.red, 2600, false);
+          return SnackArgs(
+            "Error",
+            error.message,
+            SonrIcons.Caution.white,
+            Colors.red,
+            2600,
+            false,
+            SnackPosition.TOP,
+          );
 
         // Yellow - Title Warning
         default:
           DeviceService.playSound(type: UISoundType.Warning);
-          return SnackArgs("Warning", error.message, SonrIcons.Warning.white, Colors.yellow, 2600, false);
+          return SnackArgs(
+            "Warning",
+            error.message,
+            SonrIcons.Warning.white,
+            Colors.yellow,
+            2600,
+            false,
+            SnackPosition.BOTTOM,
+          );
       }
     }
     // @ App Error
     else {
       DeviceService.playSound(type: UISoundType.Warning);
-      return SnackArgs("Error", message, SonrIcons.Caution.white, Colors.red, 2600, false);
+      return SnackArgs(
+        "Error",
+        message,
+        SonrIcons.Caution.white,
+        Colors.red,
+        2600,
+        false,
+        SnackPosition.TOP,
+      );
     }
   }
 
   /// @ Invalid Operation
   factory SnackArgs.invalid(String message) {
-    return SnackArgs("Uh Oh!", message, SonrIcons.Warning.white, Colors.orange[900], 2600, false);
+    return SnackArgs(
+      "Uh Oh!",
+      message,
+      SonrIcons.Warning.white,
+      Colors.orange[900],
+      2600,
+      false,
+      SnackPosition.BOTTOM,
+    );
   }
 
   /// @ Missing Data
   factory SnackArgs.missing(String message, {bool isLast = false}) {
     // Get Missing Title
     final list = ['Wait!', 'Hold Up!', "Uh Oh!"];
-    return SnackArgs(isLast ? "Almost There!" : list.random(), message, SonrIcons.Warning.white, SonrColor.Critical, 2600, false);
+    return SnackArgs(
+        isLast ? "Almost There!" : list.random(), message, SonrIcons.Warning.white, SonrColor.Critical, 2600, false, SnackPosition.BOTTOM);
   }
 
   /// @ Succesful Operation
   factory SnackArgs.success(String message) {
-    return SnackArgs("Success!!", message, SonrIcons.Success.white, Colors.green, 2600, true);
+    return SnackArgs(
+      "Success!!",
+      message,
+      SonrIcons.Success.white,
+      Colors.green,
+      2600,
+      true,
+      SnackPosition.BOTTOM,
+    );
   }
 }
