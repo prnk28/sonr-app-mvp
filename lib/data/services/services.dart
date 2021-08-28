3d40e6d489ae32bf47b1ac284b42c8931e5ff2ef
Prad <prnk28@gmail.com>
2021-08-27 23:45:43 -0400

    :green_heart: (ci/cd): Remove Crashlytics

--- lib/data/services/services.dart
+++ lib/data/services/services.dart
@@ -19,7 +19,6 @@ export 'package:firebase_analytics/firebase_analytics.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_messaging/firebase_messaging.dart';
 export 'package:intercom_flutter/intercom_flutter.dart';
-export 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 // Imports
 import 'package:sonr_app/env.dart';
